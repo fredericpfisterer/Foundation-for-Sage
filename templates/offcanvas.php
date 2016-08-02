d8201f25f7cefdc78bdd6f3497ed493c6b65699c
chuck <chuckn246@gmail.com>
2016-08-02 13:38:26 -0400

    Offcanvas template file

---
+++ templates/offcanvas.php
@@ -0,0 +1,7 @@
+<div class="off-canvas position-left" id="offCanvasLeft" data-off-canvas>
+	<?php get_search_form(); ?>
+	<?php sage_off_canvas_nav() ?>
+</div>
+
+<div class="off-canvas position-right" id="offCanvasRight" data-off-canvas data-position="right">
+</div>
