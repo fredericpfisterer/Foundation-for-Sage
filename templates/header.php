af924e531a7cdfb96f35ee259c4ff057e6c8b02d
chuck <chuckn246@gmail.com>
2016-08-02 12:42:51 -0400

    Fix top-bar

--- templates/header.php
+++ templates/header.php
@@ -1,20 +1,26 @@
 <header>
-  <div class="title-bar" data-responsive-toggle="top-menu" data-hide-for="medium">
-    <button class="menu-icon" type="button" data-toggle></button>
-    <div class="title-bar-title">Menu</div>
-  </div>
-  <div class="top-bar" id="top-menu">
-    <div class="top-bar-left">
-        <ul class="menu">
-          <li class="home"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo('name'); ?></a></li>
-        </ul>
-    </div>
-    <div class="top-bar-right">
-      <ul class="dropdown menu" data-dropdown-menu>
-        <?php if (has_nav_menu('primary_navigation')) :?>
-          <?php wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'container' => '', 'items_wrap' => '%3$s', 'walker' => new Roots\Sage\Extras\Foundation_Nav_Menu()]);?>
-        <?php endif;?>
-      </ul>
-    </div>
-  </div>
+  <div class="top-bar">
+    <div class="top-bar-title">
+      <span data-responsive-toggle="responsive-menu" data-hide-for="medium">
+        <button class="menu-icon dark" type="button" data-toggle></button>
+      </span>
+      <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo('name'); ?></a>
+    </div><!-- .top-bar-title -->
+
+    <div id="responsive-menu">
+      <div class="top-bar-left">
+        <ul class="dropdown menu" data-dropdown-menu>
+            <?php if (has_nav_menu('primary_navigation')) :?>
+              <?php wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'container' => '', 'items_wrap' => '%3$s', 'walker' => new Roots\Sage\Extras\Foundation_Nav_Menu()]);?>
+            <?php endif;?>
+          </ul>
+      </div><!-- .top-bar-left -->
+
+        <div class="top-bar-right">
+          <ul class="menu">
+            <li><?php get_search_form(); ?></li>
+          </ul>
+        </div><!-- .top-bar-right -->
+    </div><!-- .responsive-menu -->
+  </div><!-- .top-bar -->
 </header>
