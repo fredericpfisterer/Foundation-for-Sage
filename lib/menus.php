c51789ed653aff5269e33316fde69918ca477935
chuck <chuckn246@gmail.com>
2016-08-02 13:45:13 -0400

    Set up menus

---
+++ lib/menus.php
@@ -0,0 +1,52 @@
+<?php
+// I pieced this together from using the JointsWP Menu Code (https://github.com/JeremyEnglert/JointsWP)
+// I'm sure I should be using a Namespace here too, just not sure how to do that yet..
+
+// The Top Menu
+function sage_top_nav() {
+	wp_nav_menu([
+		'container' => false,                           // Remove nav container
+		'menu_class' => 'vertical medium-horizontal menu',       // Adding custom nav class
+		'items_wrap' => '<div class="hide-for-small-only main-menu menu-centered"><ul id="%1$s" class="%2$s" data-responsive-menu="accordion medium-dropdown" role="navigation"t>%3$s</ul></div>',
+		'theme_location' => 'primary_navigation',        			// Where it's located in the theme
+		'depth' => 5,                                   // Limit the depth of the nav
+		'fallback_cb' => false,                         // Fallback function (see below)
+		'walker' => new Topbar_Menu_Walker()
+	]);
+}
+
+// Big thanks to Brett Mason (https://github.com/brettsmason) for the awesome walker
+class Topbar_Menu_Walker extends Walker_Nav_Menu {
+	function start_lvl(&$output, $depth = 0, $args = Array() ) {
+		$indent = str_repeat("\t", $depth);
+		$output .= "\n$indent<ul class=\"menu\">\n";
+	}
+}
+// The Off Canvas Menu
+function sage_off_canvas_nav() {
+	wp_nav_menu([
+		'container' => false,                           // Remove nav container
+		'menu_class' => 'vertical menu',       // Adding custom nav class
+		'items_wrap' => '<ul id="%1$s" class="%2$s" data-accordion-menu>%3$s</ul>',
+		'theme_location' => 'primary_navigation',        			// Where it's located in the theme
+		'depth' => 5,                                   // Limit the depth of the nav
+		'fallback_cb' => false,                         // Fallback function (see below)
+		'walker' => new Off_Canvas_Menu_Walker()
+	]);
+}
+
+class Off_Canvas_Menu_Walker extends Walker_Nav_Menu {
+	function start_lvl(&$output, $depth = 0, $args = Array() ) {
+		$indent = str_repeat("\t", $depth);
+		$output .= "\n$indent<ul class=\"vertical menu\">\n";
+	}
+}
+
+// Add Foundation active class to menu
+function required_active_nav_class( $classes, $item ) {
+	if ( $item->current == 1 || $item->current_item_ancestor == true ) {
+		$classes[] = 'active';
+	}
+	return $classes;
+}
+add_filter( 'nav_menu_css_class', 'required_active_nav_class', 10, 2 );
