cd7ecf0d97d196a106c4d23122eec4ecee9d3395
Tor Raswill <tor@bereklambyra.se>
2016-10-18 02:01:01 +0200

    include new files and fixed code style

--- functions.php
+++ functions.php
@@ -13,14 +13,16 @@ $sage_includes = [
   'lib/assets.php',    // Scripts and stylesheets
   'lib/extras.php',    // Custom functions
   'lib/setup.php',     // Theme setup
+  'lib/menus.php',     // Menus Setup
+  'lib/page-navi.php', // Pagination
   'lib/titles.php',    // Page titles
   'lib/wrapper.php',   // Theme wrapper class
   'lib/customizer.php' // Theme customizer
 ];
 
-foreach ($sage_includes as $file) {
-  if (!$filepath = locate_template($file)) {
-    trigger_error(sprintf(__('Error locating %s for inclusion', 'sage'), $file), E_USER_ERROR);
+foreach ( $sage_includes as $file ) {
+  if (!$filepath = locate_template( $file) ) {
+    trigger_error( sprintf( __( 'Error locating %s for inclusion', 'sage' ), $file ), E_USER_ERROR );
   }
 
   require_once $filepath;
