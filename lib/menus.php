62ba4a50d80e725d88f27b494fbb44fd9aed722f
Chuck Nemeth <chuckn246@gmail.com>
2017-05-29 11:14:11 -0400

    code cleanup, fix namespaces. Thanks to @kosmiq

--- lib/menus.php
+++ lib/menus.php
@@ -1,22 +1,21 @@
 <?php
 // I pieced this together from using the JointsWP Menu Code (https://github.com/JeremyEnglert/JointsWP)
-// I'm sure I should be using a Namespace here too, just not sure how to do that yet..
 
 // The Top Menu
 function sage_top_nav() {
 	wp_nav_menu([
-		'container' => false,                           // Remove nav container
-		'menu_class' => 'dropdown menu',       // Adding custom nav class
-		'items_wrap' => '<div class="hide-for-small-only"><ul id="%1$s" class="%2$s" data-dropdown-menu role="navigation">%3$s</ul></div>',
-		'theme_location' => 'primary_navigation',        			// Where it's located in the theme
-		'depth' => 5,                                   // Limit the depth of the nav
-		'fallback_cb' => false,                         // Fallback function (see below)
-		'walker' => new Topbar_Menu_Walker()
+		'container'       => false,                           // Remove nav container
+		'menu_class'      => 'dropdown menu',                 // Adding custom nav class
+		'items_wrap'      => '<div class="hide-for-small-only"><ul id="%1$s" class="%2$s" data-dropdown-menu role="navigation">%3$s</ul></div>',
+		'theme_location'  => 'primary_navigation',        		// Where it's located in the theme
+		'depth'           => 5,                               // Limit the depth of the nav
+		'fallback_cb'     => false,                           // Fallback function (see below)
+		'walker'          => new Topbar_Menu_Walker()
 	]);
 }
 
 // Big thanks to Brett Mason (https://github.com/brettsmason) for the awesome walker
-class Topbar_Menu_Walker extends Walker_Nav_Menu {
+class Topbar_Menu_Walker extends \Walker_Nav_Menu {
 	function start_lvl(&$output, $depth = 0, $args = Array() ) {
 		$indent = str_repeat("\t", $depth);
 		$output .= "\n$indent<ul class=\"menu\">\n";
@@ -25,17 +24,17 @@ class Topbar_Menu_Walker extends Walker_Nav_Menu {
 // The Off Canvas Menu
 function sage_off_canvas_nav() {
 	wp_nav_menu([
-		'container' => false,                           // Remove nav container
-		'menu_class' => 'vertical menu',       // Adding custom nav class
-		'items_wrap' => '<ul id="%1$s" class="%2$s" data-accordion-menu>%3$s</ul>',
-		'theme_location' => 'primary_navigation',        			// Where it's located in the theme
-		'depth' => 5,                                   // Limit the depth of the nav
-		'fallback_cb' => false,                         // Fallback function (see below)
-		'walker' => new Off_Canvas_Menu_Walker()
+		'container'       => false,                           // Remove nav container
+		'menu_class'      => 'vertical menu',                 // Adding custom nav class
+		'items_wrap'      => '<ul id="%1$s" class="%2$s" data-accordion-menu>%3$s</ul>',
+		'theme_location'  => 'primary_navigation',        		// Where it's located in the theme
+		'depth'           => 5,                               // Limit the depth of the nav
+		'fallback_cb'     => false,                           // Fallback function (see below)
+		'walker'          => new Off_Canvas_Menu_Walker()
 	]);
 }
 
-class Off_Canvas_Menu_Walker extends Walker_Nav_Menu {
+class Off_Canvas_Menu_Walker extends \Walker_Nav_Menu {
 	function start_lvl(&$output, $depth = 0, $args = Array() ) {
 		$indent = str_repeat("\t", $depth);
 		$output .= "\n$indent<ul class=\"vertical menu\">\n";
@@ -49,4 +48,5 @@ function required_active_nav_class( $classes, $item ) {
 	}
 	return $classes;
 }
-add_filter( 'nav_menu_css_class', 'required_active_nav_class', 10, 2 );
+
+add_filter( 'nav_menu_css_class', __NAMESPACE__ . '\\required_active_nav_class', 10, 2 );
