28c61915054e162e5136e9d1d05e1a8993101f3e
Chuck Nemeth <chuckn246@gmail.com>
2017-05-29 16:12:31 -0400

    use namespace

--- templates/offcanvas.php
+++ templates/offcanvas.php
@@ -1,7 +1,7 @@
 <div class="off-canvas position-left" id="offCanvasLeft" data-off-canvas>
 	<?php get_search_form(); ?>
-	<?php if (has_nav_menu('primary_navigation')) :?>
-		<?php sage_off_canvas_nav();?>
+	<?php if ( has_nav_menu( 'primary_navigation' ) ) :?>
+    <?php Roots\Sage\Menus\sage_off_canvas_nav(); ?>
 	<?php endif;?>
 </div>
 
